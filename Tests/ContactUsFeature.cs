e77b36a7ed6e3387b0aeaafe9066761bad33d727
DieroKreator <qtechwired@gmail.com>
2025-02-13 14:05:52 -0300

    Use var keyword for type inference in ContactUsFeature test

--- Tests/ContactUsFeature.cs
+++ Tests/ContactUsFeature.cs
@@ -11,7 +11,7 @@ public class ContactUsFeature : BaseTest
     [TestProperty("Author", "Diego Rojas")]
     public void TCID2()
     {
-        ContactUsPage contactUsPage = new ContactUsPage(Driver);
+        var contactUsPage = new ContactUsPage(Driver);
         contactUsPage.GoTo();
         Assert.IsTrue(contactUsPage.IsLoaded, "The contact us page did not open successfully");
     }
