62a74395078ead064708f612aaffc6206be767a7
DieroKreator <qtechwired@gmail.com>
2025-02-15 09:52:10 -0300

    Refactor SliderFeature test to use AssertThatImageChanged for image change validation

--- Tests/SliderFeature.cs
+++ Tests/SliderFeature.cs
@@ -16,7 +16,6 @@ public class SliderFeature : BaseTest
        var currentImage = homePage.Slider.CurrentImage;
        homePage.Slider.ClickNextButton();
        var newImage = homePage.Slider.CurrentImage;
-       Assert.AreNotEqual(currentImage, newImage,
-           "The slider images did not change when pressing the next button.");
+       homePage.Slider.AssertThatImageChanged(currentImage, newImage);
     }
 }
