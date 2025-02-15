bd2853cfaeb72e4fa700ae8983eb4ca0cb46be35
DieroKreator <qtechwired@gmail.com>
2025-02-15 10:19:55 -0300

    Add SignInFunctionality test to validate navigation from Home page to Sign In page

---
+++ Tests/SignInFunctionality.cs
@@ -0,0 +1,21 @@
+using CreatingReports.Pages;
+
+namespace CreatingReports.Tests;
+
+[TestClass]
+[TestCategory("SignIn")]
+[TestCategory("SampleApp2")]
+public class SignInFunctionality : BaseTest
+{
+    [TestMethod]
+    [TestProperty("Author", "NikolayAdvolodkin")]
+    public void TCID5()
+    {
+        var homePage = new HomePage(Driver);
+        homePage.GoTo();
+        Assert.IsTrue(homePage.IsLoaded, "Home page did not open successfully");
+
+        var signInPage = homePage.Header.ClickSignInButton();
+        Assert.IsTrue(signInPage.IsLoaded, "Sign in page did not open sucessfully.");
+    }
+}
